fef7e14c81005d3f335bf5c4c00b7d081ad9f3b5
jerryq0101 <jerryq0101@gmail.com>
2022-03-13 00:17:14 -0800

    redo mainn left text.

--- src/components/Landing/Main.js
+++ src/components/Landing/Main.js
@@ -1,196 +1,47 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import './Main.css'
 
 export const Main = ({}) => {
+  
   return (
-    <TextAreaOnTheLeftRoot>
-      <ProposalDescription>
-        <SustainabilityNeedsMoneyAndTransparency>
-          Sustainability needs money.
-        </SustainabilityNeedsMoneyAndTransparency>
-        <Group>
-          <InvestingInSustainability>
-            Lets Push
-            <br />
-            Sustainability{" "}
-          </InvestingInSustainability>
-          <GreenPeriod
-            src={
-              "https://firebasestorage.googleapis.com/v0/b/rendition-prod.appspot.com/o/d1f1338e-8557-4763-8fce-438db18d3221.svg?alt=media&token=51133e60-b740-4a92-b2e4-8983dd557a44"
-            }
-          />
-        </Group>
-        <DemeterDAOIsASustainabilityfocusedInvestmentDAOThatAllowsUsersInvestInEcofriendlyProjectsAndCharities>
-          DemeterDAO is a sustainability-focused funding DAO, that makes
-          partaking in recycling easy.
-        </DemeterDAOIsASustainabilityfocusedInvestmentDAOThatAllowsUsersInvestInEcofriendlyProjectsAndCharities>
-      </ProposalDescription>
-      <ButtonDescription>
-        <Group1>
-          <FundProposals>Fund Proposals</FundProposals>
-          <HowItWorksLink>
-            <HowItWorks>How it works</HowItWorks>
-            <Line2
-              src={
-                "https://firebasestorage.googleapis.com/v0/b/rendition-prod.appspot.com/o/7783f459-cb27-42d5-a63c-02a97ef5dfd4.svg?alt=media&token=631509f0-b457-4b76-a678-e57fb053d77d"
-              }
-            />
-          </HowItWorksLink>
-        </Group1>
-        <TwoStepDescription alignItems={"stretch"}>
-          <Group34>
-            <NumberCircleOne
-              src={
-                "https://firebasestorage.googleapis.com/v0/b/rendition-prod.appspot.com/o/b7c4888b-2ad3-43b6-8f20-fcb61b61484b.svg?alt=media&token=ed74459d-ecb2-473b-8327-b084854fb44e"
-              }
-            />
-            <InvestInProjects width={"271px"}>
-              Give to treasury
-            </InvestInProjects>
-          </Group34>
-          <TwoStepDescription alignItems={"flex-end"}>
-            <NumberCircleOne
-              src={
-                "https://firebasestorage.googleapis.com/v0/b/rendition-prod.appspot.com/o/8a34929c-eed9-4c44-8068-26cdda0cf24b.svg?alt=media&token=e676cd67-0214-4b21-8934-5c0ea68f1b0e"
-              }
-            />
-            <InvestInProjects width={"291px"}>
-              Decide who to fund
-            </InvestInProjects>
-          </TwoStepDescription>
-        </TwoStepDescription>
-      </ButtonDescription>
-    </TextAreaOnTheLeftRoot>
-  );
-};
-const TwoStepDescription = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 7px;
-  align-items: ${(props) => props.alignItems};
-`;
-const NumberCircleOne = styled.img`
-  width: 32px;
-  height: 32px;
-  align-self: stretch;
-`;
-const InvestInProjects = styled.div`
-  height: 25px;
-  font-size: 18px;
-  font-family: Inter;
-  font-weight: 500;
-  margin-bottom: 3px;
-  width: ${(props) => props.width};
-`;
+    <div class="combo123">
+        <div class="flexbox-container-1 combo123-item-1">
+            <div class="flexbox-item flexbox-item-1">
+                Sustainability needs money.
+            </div>
+            <div class="flexbox-item flexbox-item-2">
+                Lets Push<br/><span>Sustainability</span><span class="green">.</span>
+            </div>
+            <div class="flexbox-item flexbox-item-3">
+                DemeterDAO is a sustainability focused DAO, that <br/>
+                makes directly supporting recycling easy.
+            </div>
+        </div>
 
-// The big css
 
-const TextAreaOnTheLeftRoot = styled.div` 
-  display: flex;
-  flex-direction: column;
-  gap: 45px;
-  margin: 0;
-  // background-color: aliceblue;
-  min-width: 650px;
-  align-items: flex-start;
-  padding: 10% 0 5% 5%;
-`;
-const ProposalDescription = styled.div`
-  height: 322px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-`;
-const SustainabilityNeedsMoneyAndTransparency = styled.div`
-  font-size: 18px;
-  font-family: Inter;
-  font-weight: 600;
-  letter-spacing: 0.54px;
-  margin-bottom: auto;
-`;
-const Group = styled.div`
-  align-self: stretch;
-  height: 185px;
-  margin-bottom: 16px;
-  width: 579px;
-  position: relative;
-`;
-const InvestingInSustainability = styled.div`
-  width: 579px;
-  height: 185px;
-  font-size: 75px;
-  font-family: Inter;
-  font-weight: 700;
-  position: absolute;
-  white-space: pre-wrap;
-`;
-const GreenPeriod = styled.img`
-  width: 12px;
-  height: 12px;
-  position: absolute;
-  top: 158px;
-  left: 505px;
-`;
-const DemeterDAOIsASustainabilityfocusedInvestmentDAOThatAllowsUsersInvestInEcofriendlyProjectsAndCharities = styled.div`
-  width: 469px;
-  height: 80px;
-  font-size: 18px;
-  font-family: Inter;
-  font-weight: 500;
-  line-height: 24px;
-`;
-const ButtonDescription = styled.div`
-  display: flex;
-  align-self: stretch;
-  flex-direction: column;
-  gap: 19px;
-`;
-const Group1 = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  gap: 69px;
-  align-items: flex-end;
-`;
-const FundProposals = styled.div`
-  color: #ffffff;
-  display: flex;
-  font-size: 24px;
-  font-family: Inter;
-  font-weight: 600;
-  align-self: stretch;
-  background-color: #219f94;
-  width: 251px;
-  height: 29px;
-  flex-direction: row;
-  justify-content: center;
-  padding-top: 19px;
-  padding-bottom: 18px;
-  border-radius: 8px;
-`;
-const HowItWorksLink = styled.div`
-  width: 151px;
-  height: 31px;
-  margin-bottom: 16px;
-  position: relative;
-`;
-const HowItWorks = styled.div`
-  color: #219f94;
-  font-size: 24px;
-  font-family: Inter;
-  font-weight: 600;
-  position: absolute;
-`;
-const Line2 = styled.img`
-  width: 151px;
-  height: 2px;
-  position: absolute;
-  top: 30px;
-  left: -1px;
-`;
-const Group34 = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  align-items: flex-end;
-`;
+        {/* <!-- Combo flexbox for 2 and 3 --> */}
+        <div class="combo123-item-2">
+            {/* <!-- Second Section Row --> */}
+            <div class="flexbox-container-2">
+                <div class="flexbox2-item flexbox2-item-1">
+                    Fund Proposals
+                </div>
+                <div class="flexbox2-item flexbox2-item-2">
+                    <div class="flexbox2-item-2-text">How it works</div>
+                </div>
+            </div>
+            {/* <!-- Third section box --> */}
+            <div class="flexbox-container-3">
+                <div class="flexbox3-item flexbox3-item-1">
+                    <img class="flexbox3-item-1-img" src="https://firebasestorage.googleapis.com/v0/b/rendition-prod.appspot.com/o/b7c4888b-2ad3-43b6-8f20-fcb61b61484b.svg?alt=media&token=ed74459d-ecb2-473b-8327-b084854fb44e"/>
+                    <div class="flexbox3-item-1-text">Give to treasury</div>
+                </div>
+                <div class="flexbox3-item flexbox3-item-2">
+                    <img class="flexbox3-item-2-img" src="https://firebasestorage.googleapis.com/v0/b/rendition-prod.appspot.com/o/8a34929c-eed9-4c44-8068-26cdda0cf24b.svg?alt=media&token=e676cd67-0214-4b21-8934-5c0ea68f1b0e"/>
+                    <div class="flexbox3-item-2-text">Decide who to fund</div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { Link} from 'react-router-dom'
 import {Connect} from './Connect.js';
 
-import './id.css'
+import './Navigation.css'
 
 export default function Navigation() {
   return (
@@ -11,7 +11,8 @@ export default function Navigation() {
       <header>
         <Link to="/">
         <div class="logo">
-            <img class="logoimg" src= "https://firebasestorage.googleapis.com/v0/b/rendition-prod.appspot.com/o/396b926f-25f3-42ab-8003-d3ce1e266bc6.svg?alt=media&token=f8cc9184-677a-44ce-9cc9-489d3f10693a" alt="logo" />
+            <img class="logo-img" src= "https://firebasestorage.googleapis.com/v0/b/rendition-prod.appspot.com/o/396b926f-25f3-42ab-8003-d3ce1e266bc6.svg?alt=media&token=f8cc9184-677a-44ce-9cc9-489d3f10693a" alt="logo" />
+            <div class="logo-text">DemeterDAO</div>
         </div>
         </Link>
          <nav>
@@ -26,117 +27,3 @@ export default function Navigation() {
     </div>
   )
 }
-
-
-// export const Navigation = ({}) => {
-//   return (
-//     <Frame2Root>
-//       <DemeterDAOMotto>
-//         <Logo
-//           src={
-//             "https://firebasestorage.googleapis.com/v0/b/rendition-prod.appspot.com/o/396b926f-25f3-42ab-8003-d3ce1e266bc6.svg?alt=media&token=f8cc9184-677a-44ce-9cc9-489d3f10693a"
-//           }
-//         />
-//         <DemeterDAO>DemeterDAO</DemeterDAO>
-        
-//       </DemeterDAOMotto>
-//       <WebsitePageSelect>
-//         <Group width={"54px"}>
-//           <Link to="/vote"><Vote left={"0px"}>Vote</Vote></Link>
-//           <Link to="/"><Vote left={"-124px"}>Home</Vote></Link>
-//           <Ellipse8
-//             src={
-//               "https://firebasestorage.googleapis.com/v0/b/rendition-prod.appspot.com/o/d9403416-7cdd-4047-aa97-5f4043c275b2.svg?alt=media&token=22b7f968-ed2c-4114-88c5-d3290ed90d68"
-//             }
-//           />
-//         </Group>
-//         <Group width={"102px"}>
-//           <Vote left={"0px"}>Treasury</Vote>
-//           <a target="_blank" href="https://demeterdao.notion.site/How-Demeter-Works-e3e041a17d354c6f8e57351363ccb419"><Vote left={"157px"}>Docs</Vote></a>
-//         </Group>
-//       </WebsitePageSelect>
-//       {/* <SignIn>Sign in</SignIn> */}
-//       <SignUp>
-//         <Connect />
-//       </SignUp>
-//     </Frame2Root>
-//   );
-// };
-
-// const Group = styled.div`
-//   height: 37px;
-//   position: relative;
-//   width: ${(props) => props.width};
-// `;
-// const Vote = styled.div`
-//   color: rgba(0, 0, 0, 0.7);
-//   font-size: 24px;
-//   font-family: Inter;
-//   font-weight: 400;
-//   position: absolute;
-//   top: -8px;
-//   left: ${(props) => props.left};
-// `;
-// const Frame2Root = styled.div`
-//   width: auto;
-//   height: auto;
-//   display: flex;
-//   flex-direction: row;
-//   margin: 3% 0 0 0;
-//   justify-content: center;
-//   align-items: flex-end;
-// `;
-// const DemeterDAOMotto = styled.div`
-//   width: 25%
-//   padding-left: 5%;
-//   display: flex;
-//   align-self: flex-start;
-  
-//   margin-right: 10%;
-// `;
-// const DemeterDAO = styled.div`
-//   font-size: 30px;
-//   font-family: Inter;
-//   font-weight: 600;
-//   padding: 10px 0 10px 15px;
-// `;
-// const Logo = styled.img`
-//   width: 52px;
-//   height: 53px;
-// `;
-// const WebsitePageSelect = styled.div`
-//   width: 338px;
-//   margin-left: auto;
-//   margin-right: auto;
-//   display: flex;
-//   flex-direction: row;
-//   gap: 55px;
-// `;
-// const Ellipse8 = styled.img`
-//   width: 7px;
-//   height: 7px;
-//   position: absolute;
-//   top: 30px;
-//   left: -93px;
-// `;
-// const SignIn = styled.div`
-//   font-size: 24px;
-//   font-family: Inter;
-//   font-weight: 600;
-//   padding: 14px 50px 14px 50px;
-//   margin-right: 2em;
-// `;
-// const SignUp = styled.div`
-//   color: 0;
-//   display: flex;
-//   font-size: 24px;
-//   font-family: Inter;
-//   font-weight: 600;
-//   // background-color: #219f94;
-  
-//   flex-direction: row;
-//   justify-content: center;
-//   padding: 14px 50px 14px 2%;
-//   border-radius: 8px;
-//   margin-right: 5%;
-// `;
